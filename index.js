ee5a57dfe5bf15664204ac6d6bb3a5cf2005d1d0
chrispevery <chris.p.every@gmail.com>
2021-01-31 00:34:28 -0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!')
+})
+
+client.on('message', message => {
+	//console.log(message.content);
+
+	if(message.content.startsWith('${prefix}addChannel')) {
+		message.channel.send("Creating Channel")
+	}
+})
+
+client.login(token);
